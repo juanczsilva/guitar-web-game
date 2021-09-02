5199470d3e9da923f9b409d61d268ff859774221
JuAnY <juany@juany.com>
2021-09-02 19:13:25 -0300

    .

--- web_client/cl.js
+++ web_client/cl.js
@@ -1,3 +1,28 @@
+// YOUTUBE
+var tag = document.createElement('script');
+tag.src = "http://www.youtube.com/player_api";
+
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+let player;
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    events: {
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+function onPlayerStateChange(event) {
+  console.log(event);
+  if (event.data == YT.PlayerState.PLAYING) {
+    player.unMute();
+  }
+}
+
+// GAME
 const sock = io();
 
 sock.on("msg", (text) => {
